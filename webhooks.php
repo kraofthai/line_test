1c439397ec95f17158733a0949c97a1a40853b08
kraofthai <46700371+kraofthai@users.noreply.github.com>
2019-01-15 13:31:28 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			//$url = 'https://api.line.me/v2/bot/message/reply';
 			//$url = 'https://th.wikipedia.org/w/api.php';
 			$data = [
 				'replyToken' => $replyToken,
